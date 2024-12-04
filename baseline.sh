f758de71643e592139567c179e4bc63515016efa
Xin Wang <xin874@ucsb.edu>
2024-12-03 20:17:10 -0600

    ready to run gcn baseline

--- baseline.sh
+++ baseline.sh
@@ -1,1 +1,17 @@
 python finetune_baseline.py --dataset AID1798 --split random_cv2
+
+python finetune_baseline.py --dataset AID1798 --split random_cv3
+
+python finetune_baseline.py --dataset AID1798 --split random_cv4
+
+python finetune_baseline.py --dataset AID1798 --split random_cv5
+
+python finetune_baseline.py --dataset AID1798 --split scaffold_seed1
+
+python finetune_baseline.py --dataset AID1798 --split scaffold_seed2
+
+python finetune_baseline.py --dataset AID1798 --split scaffold_seed3
+
+python finetune_baseline.py --dataset AID1798 --split scaffold_seed4
+
+python finetune_baseline.py --dataset AID1798 --split scaffold_seed5
